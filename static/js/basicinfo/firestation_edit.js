494e16b11899ea45a62248833826d4e493cfb27e
rliu666 <rliu666@sina.cn>
2018-07-30 10:56:54 +0800

    新增修改校验

--- static/js/basicinfo/firestation_edit.js
+++ static/js/basicinfo/firestation_edit.js
@@ -312,7 +312,7 @@ new Vue({
             }
         },
         //保存前校验
-        validateSave: function(){
+        validateSave: function(){   
             if (this.editForm.dzmc == "" || this.editForm.dzmc == null) {
                 this.$message.warning({
                     message: '请输入队站名称',
@@ -325,6 +325,38 @@ new Vue({
                     showClose: true
                 });
                 return false;
+            }else if(!this.validateNum(this.editForm.lon, "经度应为数值型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.lat, "纬度应为数值型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.gisX, "GIS_X应为数值型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.gisY, "GIS_Y应为数值型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.gxsys, "管辖水源数应为数值型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.gxzddws, "管辖重点单位数应为数值型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.xqmj, "执勤车辆数应为整数型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.zbqcs, "装备器材数应为整数型")){
+                return false;
+            }else if(!this.validateNum(this.editForm.mhjzl, "灭火剂总量应为数值型")){
+                return false;
+            }
+            return true;
+        },
+        //数值型校验
+        validateNum: function(val, message){
+            var regPos = /^\d+(\.\d+)?$/; //非负浮点数
+            if(val!="" && val!=null){
+                if(!regPos.test(val)){
+                    this.$message.warning({
+                        message: message,
+                        showClose: true
+                    });
+                    return false;
+                }              
             }
             return true;
         },
@@ -339,6 +371,8 @@ new Vue({
                                 showClose: true
                             });
                         } else {
+                            console.log(44444444);
+                            console.log(this.editForm);
                             this.editForm.cjrid = this.shiroData.userid;
                             this.editForm.cjrmc = this.shiroData.realName;
                             this.editForm.dzlx = this.editForm.dzlx[this.editForm.dzlx.length-1];
