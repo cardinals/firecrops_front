1d826c9780b49cda8e6af83ddddb464f917a3f02
lixue0919 <442019696@qq.com>
2018-09-10 14:51:59 +0800

    确认密码，要求必填

--- static/js/system/user.js
+++ static/js/system/user.js
@@ -65,6 +65,10 @@ var vue = new Vue({
                     { required: true, message: '请输入密码', trigger: 'blur' },
                     { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
                 ],
+                checkPass: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
+                ],
                 organizationId: [
                     { required: true, message: '请选择组织机构', trigger: 'blur' }
                 ]
