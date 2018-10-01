638fabd4dfda5ebbc382340903ae1e3683f801b7
huang-rui <hr18904047625@163.com>
2018-10-01 14:18:35 +0800

    问题列表修改完毕

--- static/js/jxcsplan/jxcsplan_approve.js
+++ static/js/jxcsplan/jxcsplan_approve.js
@@ -11,13 +11,14 @@ var vue = new Vue({
                 dwmc: "",
                 jxdwlx: "",
                 dwdz: "",
-                xfgx: []
+                xfgx: [],
+                shzt: "未审核",
             },
             tableData: [],//列表信息
             shiroData: [],//当前用户信息
             XFGX_dataTree: [],//消防管辖级联选择
             JXDWLX_data: [],//九小单位类型下拉框
-
+            SHZT_data: [],//审核状态下拉框
             //表高度变量
             tableheight: 443,
             //显示加载中样
@@ -44,11 +45,11 @@ var vue = new Vue({
             },
             radio: "",
             //未通过flag
-            isReject:false,
+            isReject: false,
             //审批表单
             approveForm: {
                 shzt: -1,
-                reserve1:""
+                reserve1: ""
             },
             approveFormVisible: false,
         }
@@ -58,11 +59,11 @@ var vue = new Vue({
         this.shiroData = shiroGlobal;
         this.XFGX();//消防管辖级联选择
         this.JXDWLX();//九小单位类型下拉框
+        this.SHZT();//审核状态下拉框
     },
     mounted: function () {
         this.searchClick('click');//条件查询
     },
-
     methods: {
         //消防管辖级联选择
         XFGX: function () {
@@ -86,6 +87,15 @@ var vue = new Vue({
                 console.log(error);
             })
         },
+        //审核状态下拉框
+        SHZT: function () {
+            axios.get('/api/codelist/getCodetype/YASHZT').then(function (res) {
+                this.SHZT_data = res.data.result;
+                this.searchForm.shzt = (this.SHZT_data[0].codeValue);
+            }.bind(this), function (error) {
+                console.log(error);
+            })
+        },
         //表格查询事件
         searchClick: function (type) {
             //按钮事件的选择
@@ -96,9 +106,15 @@ var vue = new Vue({
                 this.currentPage = 1;
             }
             this.loading = true;//表格重新加载
+            var shztbm = "";
+            if (this.searchForm.shzt == "未审核") {
+                shztbm = "01";
+            } else {
+                shztbm = this.searchForm.shzt;
+            }
             //add by yushch 中队显示总队所有数据bug解决
             var xfgx = "";
-            if(this.shiroData.organizationVO.jgid.substr(2,6)!='000000'){
+            if (this.shiroData.organizationVO.jgid.substr(2, 6) != '000000') {
                 xfgx = this.shiroData.organizationVO.uuid;
             }
             //end 20180929
@@ -107,14 +123,15 @@ var vue = new Vue({
                 dwdz: this.searchForm.dwdz,
                 xfgx: this.searchForm.xfgx[this.searchForm.xfgx.length - 1],
                 jxdwlx: this.searchForm.jxdwlx,
+                shzt: shztbm,
                 pageSize: this.pageSize,
                 pageNum: this.currentPage,
                 orgUuid: this.shiroData.organizationVO.uuid,
                 orgJgid: this.shiroData.organizationVO.jgid,
-                jdh: this.shiroData.organizationVO.jgid.substr(0,2)+'000000',
-                xfgx:xfgx
+                jdh: this.shiroData.organizationVO.jgid.substr(0, 2) + '000000',
+                // xfgx:xfgx
             }
-            axios.post('/dpapi/jxcsjbxx/page', params).then(function (res) {
+            axios.post('/dpapi/jxcsjbxx/listForApprove', params).then(function (res) {
                 var tableTemp = new Array((this.currentPage - 1) * this.pageSize);
                 this.tableData = tableTemp.concat(res.data.result.list);
                 this.total = res.data.result.total;
@@ -153,11 +170,11 @@ var vue = new Vue({
         //编辑跳转
         handleEdit: function (row) {
             // if (row.yazt == '01' || row.yazt == '04') {
-                var params = {
-                    ID: row.uuid,
-                    type: "BJ"
-                }
-                loadDivParam("jxcsplan/jxcsplan_add", params);
+            var params = {
+                ID: row.uuid,
+                type: "BJ"
+            }
+            loadDivParam("jxcsplan/jxcsplan_add", params);
             // } else {
             //     this.$message({
             //         message: "仅编辑中和已驳回状态预案可编辑",
@@ -224,15 +241,14 @@ var vue = new Vue({
                 });
                 return;
             }
-
-            if (this.tableData[this.data_index].shzt == '03') {
-                this.$message({
-                    message: "该记录已审核通过！",
-                    type: "warning",
-                    showClose: true
-                });
-                return;
-            }
+            // if (this.tableData[this.data_index].shzt == '03') {
+            //     this.$message({
+            //         message: "该记录已审核通过！",
+            //         type: "warning",
+            //         showClose: true
+            //     });
+            //     return;
+            // }
             //获取预案uuid
             var row = this.tableData[this.data_index];
             this.uuid = row.uuid;
@@ -245,19 +261,31 @@ var vue = new Vue({
             });
             this.approveForm = Object.assign({}, row);
             //如果是未通过审核意见显示*代表必填
-            if(this.approveForm.shzt == '02')
+            if (this.approveForm.shzt == '02')
                 this.isReject = true;
             this.approveFormVisible = true;
         },
         //保存点击事件
         approveSubmit: function (val) {
-            if(this.isReject==true && val.reserve1 == null)
+            if (this.isReject == false) {
+                this.$message({
+                    message: "请选择审核状态",
+                    type: "error",
+                    showClose: true
+                });
+            } else if (this.isReject == true && val.reserve1 == null) {
                 this.$message({
                     message: "请填写审核意见",
                     type: "error",
                     showClose: true
                 });
-            else{
+            } else if (validateBytes(val.reserve1, 36)) {
+                this.$message({
+                    message: "字段超长，请重新输入",
+                    type: "error",
+                    showClose: true
+                });
+            } else {
                 //审核状态改变才调用后台approveByVO方法
                 if (val.shzt == this.tableData[this.data_index].shzt && val.reserve1 == this.tableData[this.data_index].reserve1) {
                     this.$message({
@@ -281,19 +309,19 @@ var vue = new Vue({
                             type: 'success',
                             confirmButtonText: '确定'
                         });
+                        this.data_index = 0;
+                        this.radio = 0;
                     }.bind(this), function (error) {
                         console.log(error)
                     })
                     this.approveFormVisible = false;
                     this.loadingData();
                 }
-
             }
-  
         },
         //审核状态为未通过时审核意见显示*代表必填
-        radioChange:function(){
-            if(this.approveForm.shzt == '02')
+        radioChange: function () {
+            if (this.approveForm.shzt == '02')
                 this.isReject = true;
             else
                 this.isReject = false;
@@ -307,7 +335,6 @@ var vue = new Vue({
                 console.info("加载数据成功");
                 _self.loading = false;
             }, 300);
-        },
-    },
-
+        }
+    }
 })
