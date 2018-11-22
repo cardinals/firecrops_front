c3e4e332d7e557b906fd15a6ce145dbd9350bb19
lixue0919 <442019696@qq.com>
2018-11-22 10:22:44 +0800

    九小场所登陆流程，取消引用all.js，在jxcs_all.js中增加登陆用户信息  by li.xue 2018/11/22

--- static/js/jxcsplan/jxcsplan_all.js
+++ static/js/jxcsplan/jxcsplan_all.js
@@ -6,7 +6,31 @@ $(function () {
     }
     loadDiv("jxcsplan/jxcsplan_add");
 });
-
+/**header-box by li.xue 20180628 */
+var shiroGlobal = "";
+var realname = "";
+var permissions = [];
+axios.get('/api/shiro').then(function (res) {
+    if(res.data.organizationVO == null || res.data.organizationVO == ""){
+        res.data.organizationVO = {
+            uuid: "",
+            jgjc: "",
+            jgid: ""
+        }
+    }
+    shiroGlobal = res.data;
+    //用户权限
+    for(var i in res.data.permissions){
+        permissions.push(res.data.permissions[i]);
+    }
+    realname = res.data.realName;
+    document.querySelector("#realname").innerHTML = realname;
+    if(res.data == null && realname == null && realname == ""){
+        window.location.href = "login.html";
+    }
+}.bind(this), function (error) {
+    console.log(error)
+});
 //退出登录
 function logout(){
     $('#login-out-form')[0].submit();
